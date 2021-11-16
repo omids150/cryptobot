53668af7d795f701a8425fbfb05ac3cb7ae99490
omid <omid.sadegi2003@gmail.com>
2021-11-16 10:05:41 +0100

    beginne

--- legtest.py
+++ legtest.py
@@ -6,19 +6,25 @@ import requests
 def get_main_coins():
     #btc
     url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=eur&days=max&interval=minutes%5"
-    response_btc = requests.request("GET", url)    
-    
+    response_btc = requests.request("GET", url)
+    btc_df = pd.DataFrame(response_btc.json()["prices"],columns=["ts","price"])
+    btc_df["ts"] = pd.to_datetime(btc_df["ts"])
+    btc_df.set_index("ts")
+
     #eth
     url = "https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=eur&days=max&interval=minutes%5"
     response_eth = requests.request("GET", url)
-    
-
-    btc_df = pd.DataFrame(response_btc.json()["prices"],columns=["ts","price"],index="prices")
     eth_df = pd.DataFrame(response_eth.json()["prices"],columns=["ts","price"])
+    eth_df["ts"] = pd.to_datetime(eth_df["ts"])
+    eth_df.set_index("ts")
 
     return btc_df,eth_df
 
+def avalable_currencyes():
+    res = requests.request("GET","https://api.coingecko.com/api/v3/coins/list")
+    res = res.json()
+    return pd.DataFrame(res)
 
 btc_df,eth_df = get_main_coins()
+coin_list = avalable_currencyes()
 
-print(btc_df)
