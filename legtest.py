7b0c58b7bffaed78995a196c437ee74c2b1d080a
Omid Sadegi <omid.sadegi2003@gmail.com>
2021-11-16 17:23:53 +0100

    progress

--- legtest.py
+++ legtest.py
@@ -5,54 +5,64 @@ import requests
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import datetime as datetime
 
-
-def get_main_coins():
-
-    url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=eur&days=max&interval=minutes%5"
-    response_btc = requests.request("GET", url)
-    btc_df = pd.DataFrame(response_btc.json()["prices"],columns=["ts","price"])
-    btc_df["ts"] = pd.to_datetime(btc_df["ts"])
-    btc_df.set_index("ts")
-    btc_df["log_price"] = np.log(btc_df["price"])
-
-    # eth
-    url = "https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=eur&days=max&interval=minutes%5"
-    response_eth = requests.request("GET", url)
-    eth_df = pd.DataFrame(response_eth.json()["prices"],columns=["ts","price"])
-    eth_df["ts"] = pd.to_datetime(eth_df["ts"])
-    eth_df.set_index("ts")
-    eth_df["log_price"] = np.log(eth_df["price"])
-
-    return btc_df,eth_df
-
-def get_coin_by_name(name):
+def get_coin_by_name(name,start="max"):
     # get coin by name 
-    url = f"https://api.coingecko.com/api/v3/coins/{name}/market_chart?vs_currency=eur&days=max&interval=minutes%5"
+    url = f"https://api.coingecko.com/api/v3/coins/{name}/market_chart?vs_currency=eur&days={start}&interval=minute%5" # -> warum bekomme ich nur Tägliche daten  ?
     res = requests.request("GET", url)
     coin_df = pd.DataFrame(res.json()["prices"],columns=["ts","price"])
-    coin_df["ts"] = pd.to_datetime(coin_df["ts"])
+    coin_df["ts"] = pd.to_datetime(coin_df["ts"].div(1000.0), unit="s") # -> nicht ganz sicher ob das stimmt 
     coin_df.set_index("ts")
-    coin_df["log_price"] = np.log(coin_df["price"])
+
+    # Use min max Scaler on coins 
+    scaler = MinMaxScaler()
+    price_array = np.matrix(coin_df["price"])
+
+    scaled_price = scaler.fit_transform(price_array)
+
+    coin_df["scaled_price"] = scaled_price[0]
 
     return  coin_df 
 
+def get_main_coins(names=["bitcoin","ethereum"]):
+
+    coin_dict = {}
+
+    for n in names: 
+        coin_df = get_coin_by_name(n)
+        coin_dict[n] = coin_df
+    
+    return coin_dict
+
 def avalable_currencyes():
-    # get all avalable coin 
+    # get all avalable coin on coingecko 
     res = requests.request("GET","https://api.coingecko.com/api/v3/coins/list")
     res = res.json()
     return pd.DataFrame(res)
 
-def plot_chart(coin1,coin2=None,Name=""):
-    fig = px.line(coin1, x="ts", y="log_price",title="")
-    try: 
+def plot_chart(coin1 ,coin2=None ,Name=""):
+    #plot two coins 
+    fig = px.line(coin1, x="ts", y="price",title="")
+    try:
         bool(coin2 != None )
     except:
         fig.add_trace(go.Scatter(x=coin2["ts"], y=coin2["price"]))
     fig.show()
 
+###############################################################################################
+####################### ACTUAL CODE 
+###############################################################################################
 
-btc_df,eth_df = get_main_coins()
+#get bitcoin and etherium data 
+main_coin_dict = get_main_coins()
+btc_df = main_coin_dict["bitcoin"]
+eth_df = main_coin_dict["ethereum"]
+
+print(btc_df)
+
+#get coin to compare 
 rand_coin = get_coin_by_name("decentraland")
 
 plot_chart(btc_df,rand_coin)
