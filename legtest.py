3af27d4a0dcf258eab1e524d72154e316860d492
omid <omid.sadegi2003@gmail.com>
2021-11-16 10:39:34 +0100

    a

--- legtest.py
+++ legtest.py
@@ -1,17 +1,18 @@
 import pandas as pd
-import requests
+import requests 
+import plotly.express as px
+import plotly.graph_objects as go
 
-#key 8d661ff9196adcc59e493c20c375732a8e3a101f
 
 def get_main_coins():
-    #btc
+    # btc
     url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=eur&days=max&interval=minutes%5"
     response_btc = requests.request("GET", url)
     btc_df = pd.DataFrame(response_btc.json()["prices"],columns=["ts","price"])
     btc_df["ts"] = pd.to_datetime(btc_df["ts"])
     btc_df.set_index("ts")
 
-    #eth
+    # eth
     url = "https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=eur&days=max&interval=minutes%5"
     response_eth = requests.request("GET", url)
     eth_df = pd.DataFrame(response_eth.json()["prices"],columns=["ts","price"])
@@ -20,11 +21,28 @@ def get_main_coins():
 
     return btc_df,eth_df
 
+def get_coin_by_name(name):
+    # get coin by name 
+    url = f"https://api.coingecko.com/api/v3/coins/{name}/market_chart?vs_currency=eur&days=max&interval=minutes%5"
+    res = requests.request("GET", url)
+    coin_df = pd.DataFrame(res.json()["prices"],columns=["ts","price"])
+    coin_df["ts"] = pd.to_datetime(coin_df["ts"])
+    coin_df.set_index("ts")
+    return  coin_df 
+
 def avalable_currencyes():
+    # get all avalable coin 
     res = requests.request("GET","https://api.coingecko.com/api/v3/coins/list")
     res = res.json()
     return pd.DataFrame(res)
 
-btc_df,eth_df = get_main_coins()
-coin_list = avalable_currencyes()
+def plot_chart(coin1,coin2=None):
+    fig = px.line(coin1, x="ts", y="price", title='Life expectancy in Canada')
+    if coin2 != None: 
+        fig.add_trace(go.Scatter(x=coin2["ts"], y=coin2["price"]))
+    fig.show()
 
+btc_df,eth_df = get_main_coins()
+rand_coin = get_coin_by_name("decentraland")
+print(rand_coin)
+plot_chart(btc_df,rand_coin)
