f89258435f3d1550829bb1c5d8aa6afd3c497166
omid <omid.sadegi2003@gmail.com>
2021-11-17 12:42:46 +0100

    hallo

--- legtest.py
+++ legtest.py
@@ -5,12 +5,13 @@ import requests
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
+from requests.models import RequestHooksMixin
 from sklearn.preprocessing import MinMaxScaler
 import datetime as datetime
 
-def get_coin_by_name(name,start="max"):
+def get_coin_by_name(name,time="max"):
     # get coin by name 
-    url = f"https://api.coingecko.com/api/v3/coins/{name}/market_chart?vs_currency=eur&days={start}&interval=minute%5" # -> warum bekomme ich nur Tägliche daten  ?
+    url = f"https://api.coingecko.com/api/v3/coins/{name}/market_chart?vs_currency=eur&days={time}&interval=minutly" # -> warum bekomme ich nur Tägliche daten  ?
     res = requests.request("GET", url)
     coin_df = pd.DataFrame(res.json()["prices"],columns=["ts","price"])
     coin_df["ts"] = pd.to_datetime(coin_df["ts"].div(1000.0), unit="s") # -> nicht ganz sicher ob das stimmt 
@@ -18,20 +19,22 @@ def get_coin_by_name(name,start="max"):
 
     # Use min max Scaler on coins 
     scaler = MinMaxScaler()
-    price_array = np.matrix(coin_df["price"])
+    price_array = np.array(coin_df["price"]).reshape(-1,1)
 
     scaled_price = scaler.fit_transform(price_array)
+    # print(scaled_price)
+    scaled_price = [float(i) for i in scaled_price] #-> problem 
 
     coin_df["scaled_price"] = scaled_price[0]
 
     return  coin_df 
 
-def get_main_coins(names=["bitcoin","ethereum"]):
+def get_main_coins(names=["bitcoin","ethereum"],time="max"):
 
     coin_dict = {}
 
     for n in names: 
-        coin_df = get_coin_by_name(n)
+        coin_df = get_coin_by_name(n,time=time)
         coin_dict[n] = coin_df
     
     return coin_dict
@@ -44,11 +47,11 @@ def avalable_currencyes():
 
 def plot_chart(coin1 ,coin2=None ,Name=""):
     #plot two coins 
-    fig = px.line(coin1, x="ts", y="price",title="")
+    fig = px.line(coin1, x="ts", y="scaled_price",title="")
     try:
         bool(coin2 != None )
     except:
-        fig.add_trace(go.Scatter(x=coin2["ts"], y=coin2["price"]))
+        fig.add_trace(go.Scatter(x=coin2["ts"], y=coin2["scaled_price"]))
     fig.show()
 
 ###############################################################################################
@@ -56,13 +59,14 @@ def plot_chart(coin1 ,coin2=None ,Name=""):
 ###############################################################################################
 
 #get bitcoin and etherium data 
-main_coin_dict = get_main_coins()
+main_coin_dict = get_main_coins(time=30)
 btc_df = main_coin_dict["bitcoin"]
 eth_df = main_coin_dict["ethereum"]
 
-print(btc_df)
-
 #get coin to compare 
-rand_coin = get_coin_by_name("decentraland")
+rand_coin = get_coin_by_name("decentraland",time=30)
 
-plot_chart(btc_df,rand_coin)
+#plot_chart(btc_df,rand_coin)
+
+print(btc_df)
+print(eth_df)
