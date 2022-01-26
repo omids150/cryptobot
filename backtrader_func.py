51f4f5eb09883bec93001c5531255dbe42d0548a
Omid Sadegi <omid.sadegi2003@gmail.com>
2022-01-26 19:48:30 +0100

    wie zum fick kann man daten hinzufügen ??

--- backtrader_func.py
+++ backtrader_func.py
@@ -1,21 +1,17 @@
 import backtrader as bt
-from numpy.testing._private.utils import print_assert_equal
 
 import lagtestV2 as lg 
 
-class MyInd(bt.Indicator):
-    lines = ('ind',)
 
-    def __init__(self):
-        main_coins = lg.get_main_coins_eod()
-        join = lg.joinTimeSeries(main_coins)
+def get_scaled_price():
+    main_coins = lg.get_main_coins_eod()
+    join = lg.joinTimeSeries(main_coins)
 
-        join = join.drop(columns = ["BTC","ETH","ADA","XRP","SOL","returns","rolling","scaled_price_std"])
+    #join = join.drop(columns = ["BTC","ETH","ADA","XRP","SOL","returns","rolling","scaled_price_std"]) #scaled_price
+    join = join.drop(columns = ["ETH","ADA","XRP","SOL","returns","rolling","scaled_price_std"]) #scaled_price
 
-        print(join)
-        data = bt.feeds.PandasData(dataname=join)
+    return join
 
-        self.lines.ind = data
 
 # Create a Stratey
 class TestStrategy(bt.Strategy):
@@ -28,10 +24,9 @@ class TestStrategy(bt.Strategy):
     def __init__(self):
         # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
-        self.myInd = MyInd()
-
+        #self.scale = self.datas[0].scaled_data
     def next(self):
         # Simply log the closing price of the series from the reference
-        self.log('Close, %.2f' % self.dataclose[0])
+        pass
+        #self.log('Close, %.2f' % self.scale )
         
-        # self.log(self.myInd)
